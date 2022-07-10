cd449e44d3ba2d0873a94a6115e7df208492a63f
fanghaoming <fanghaoming@rayvision.com>
2022-07-10 20:57:24 +0800

    auto issue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -82,6 +82,7 @@ module.exports = {
             repo: 'web-blog',
             clientId: 'd8808fb381f7e94ba7c5',
             clientSecret: '96657e87462d5286b26a80fb19617a0d5392aae6',
+            autoCreateIssue: true,
         },
     },
 };
