a22475a7bcb74c312fe681967e0ba0a0350e7b08
fanghaoming <fanghaoming@rayvision.com>
2022-07-10 22:25:42 +0800

    aa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -81,7 +81,7 @@ module.exports = {
     plugins: {
         '@vssue/vuepress-plugin-vssue': {
             platform: 'github',
-            owner: 'Fanghaoming',
+            owner: 'FangHaoming',
             repo: 'web-blog',
             clientId: 'd8808fb381f7e94ba7c5',
             clientSecret: '96657e87462d5286b26a80fb19617a0d5392aae6',
