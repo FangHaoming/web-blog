24836cead9f066a44685ca94e6126ab4ae5b174b
fanghaoming <fanghaoming@rayvision.com>
2022-07-20 14:32:50 +0800

    ✨ add axios

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,24 +1,10 @@
 <template>
-  <div
-    class="theme-container"
-    :class="pageClasses"
-    @touchstart="onTouchStart"
-    @touchend="onTouchEnd"
-  >
-    <Navbar
-      v-if="shouldShowNavbar"
-      @toggle-sidebar="toggleSidebar"
-    />
+  <div class="theme-container" :class="pageClasses" @touchstart="onTouchStart" @touchend="onTouchEnd">
+    <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar" />
 
-    <div
-      class="sidebar-mask"
-      @click="toggleSidebar(false)"
-    />
+    <div class="sidebar-mask" @click="toggleSidebar(false)" />
 
-    <Sidebar
-      :items="sidebarItems"
-      @toggle-sidebar="toggleSidebar"
-    >
+    <Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar">
       <template #top>
         <slot name="sidebar-top" />
       </template>
@@ -29,10 +15,7 @@
 
     <Home v-if="$page.frontmatter.home" />
 
-    <Page
-      v-else
-      :sidebar-items="sidebarItems"
-    >
+    <Page v-else :sidebar-items="sidebarItems">
       <template #top>
         <slot name="page-top" />
       </template>
@@ -45,120 +28,142 @@
 </template>
 
 <script>
-import Home from '@theme/components/Home.vue'
-import Navbar from '@theme/components/Navbar.vue'
-import Page from '@theme/components/Page.vue'
-import Sidebar from '@theme/components/Sidebar.vue'
-import { resolveSidebarItems } from './util'
+  import Home from '@theme/components/Home.vue'
+  import Navbar from '@theme/components/Navbar.vue'
+  import Page from '@theme/components/Page.vue'
+  import Sidebar from '@theme/components/Sidebar.vue'
+  import { resolveSidebarItems } from './util'
+  import axios from 'axios'
+  import getAxios from '../../utils/getAxios.js'
 
-export default {
-  name: 'Layout',
+  export default {
+    name: 'Layout',
 
-  components: {
-    Home,
-    Page,
-    Sidebar,
-    Navbar
-  },
-
-  data () {
-    return {
-      isSidebarOpen: false,
-      issueTitle: ''
-    }
-  },
-
-  computed: {
-    shouldShowNavbar () {
-      const { themeConfig } = this.$site
-      const { frontmatter } = this.$page
-      if (
-        frontmatter.navbar === false
-        || themeConfig.navbar === false) {
-        return false
-      }
-      return (
-        this.$title
-        || themeConfig.logo
-        || themeConfig.repo
-        || themeConfig.nav
-        || this.$themeLocaleConfig.nav
-      )
+    components: {
+      Home,
+      Page,
+      Sidebar,
+      Navbar
     },
 
-    shouldShowSidebar () {
-      const { frontmatter } = this.$page
-      return (
-        !frontmatter.home
-        && frontmatter.sidebar !== false
-        && this.sidebarItems.length
-      )
-    },
-
-    sidebarItems () {
-      return resolveSidebarItems(
-        this.$page,
-        this.$page.regularPath,
-        this.$site,
-        this.$localePath
-      )
-    },
-
-    pageClasses () {
-      const userPageClass = this.$page.frontmatter.pageClass
-      return [
-        {
-          'no-navbar': !this.shouldShowNavbar,
-          'sidebar-open': this.isSidebarOpen,
-          'no-sidebar': !this.shouldShowSidebar
-        },
-        userPageClass
-      ]
-    }
-  },
-
-  mounted () {
-    this.setIssueTitle()
-    this.$router.afterEach(() => {
-      this.isSidebarOpen = false
-      this.setIssueTitle()
-      
-    })
-  },
-
-  methods: {
-    setIssueTitle() {
-      const url = decodeURI(window.document.location.toString())
-      if(!url.includes('html')){
-        this.issueTitle = url.split('/')[url.split('/').length-2]
-      } else{
-        this.issueTitle = url.substring(url.lastIndexOf('/')+1, url.lastIndexOf('.html'))
-      }
-    },
-    toggleSidebar (to) {
-      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-      this.$emit('toggle-sidebar', this.isSidebarOpen)
-    },
-
-    // side swipe
-    onTouchStart (e) {
-      this.touchStart = {
-        x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
+    data() {
+      return {
+        isSidebarOpen: false,
+        issueTitle: ''
       }
     },
 
-    onTouchEnd (e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
-      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-        if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
-        } else {
-          this.toggleSidebar(false)
+    computed: {
+      shouldShowNavbar() {
+        const { themeConfig } = this.$site
+        const { frontmatter } = this.$page
+        if (
+          frontmatter.navbar === false
+          || themeConfig.navbar === false) {
+          return false
         }
+        return (
+          this.$title
+          || themeConfig.logo
+          || themeConfig.repo
+          || themeConfig.nav
+          || this.$themeLocaleConfig.nav
+        )
+      },
+
+      shouldShowSidebar() {
+        const { frontmatter } = this.$page
+        return (
+          !frontmatter.home
+          && frontmatter.sidebar !== false
+          && this.sidebarItems.length
+        )
+      },
+
+      sidebarItems() {
+        return resolveSidebarItems(
+          this.$page,
+          this.$page.regularPath,
+          this.$site,
+          this.$localePath
+        )
+      },
+
+      pageClasses() {
+        const userPageClass = this.$page.frontmatter.pageClass
+        return [
+          {
+            'no-navbar': !this.shouldShowNavbar,
+            'sidebar-open': this.isSidebarOpen,
+            'no-sidebar': !this.shouldShowSidebar
+          },
+          userPageClass
+        ]
+      }
+    },
+
+    mounted() {
+      this.$router.beforeEach(async (to, from, next) => {
+        if (this.getAuth()) {
+          const res = await this.getGithubAxios().get('/user')
+          console.dir(res)
+        }
+        next()
+      })
+      this.$router.afterEach(() => {
+        this.isSidebarOpen = false
+        this.setIssueTitle()
+        // axios.get('https://api.github.com/repos/FangHaoming/web-blog/issues/18/comments').then((res) => {
+        //   console.dir(res)
+        // })
+      })
+    },
+
+    methods: {
+      setIssueTitle() {
+        const url = decodeURI(window.document.location.toString())
+        if (!url.includes('html')) {
+          this.issueTitle = url.split('/')[url.split('/').length - 2]
+        } else {
+          this.issueTitle = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.html'))
+        }
+      },
+      toggleSidebar(to) {
+        this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
+        this.$emit('toggle-sidebar', this.isSidebarOpen)
+      },
+
+      // side swipe
+      onTouchStart(e) {
+        this.touchStart = {
+          x: e.changedTouches[0].clientX,
+          y: e.changedTouches[0].clientY
+        }
+      },
+      onTouchEnd(e) {
+        const dx = e.changedTouches[0].clientX - this.touchStart.x
+        const dy = e.changedTouches[0].clientY - this.touchStart.y
+        if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+          if (dx > 0 && this.touchStart.x <= 80) {
+            this.toggleSidebar(true)
+          } else {
+            this.toggleSidebar(false)
+          }
+        }
+      },
+      getAuth() {
+        return localStorage.getItem('Vssue.github.access_token')
+      },
+      getGithubAxios() {
+        return getAxios(
+          axios.create({
+            baseURL: 'https://api.github.com/',
+            headers: {
+              authorization: 'token ' + localStorage.getItem('Vssue.github.access_token')
+            }
+          })
+        )
       }
     }
   }
-}
 </script>
---
+++ docs/.vuepress/utils/getAxios.js
@@ -0,0 +1,28 @@
+export default function (instance) {
+  return {
+    get: function (url, params = {}, config = {}) {
+      return instance({
+        method: 'get',
+        url,
+        params,
+        ...config,
+      }).then((response) => {
+        return Promise.resolve(response)
+      }).catch((error) => {
+        return Promise.reject(error)
+      })
+    },
+    post: function (url, data = {}, config = {}) {
+      return instance({
+        method: 'post',
+        url,
+        data,
+        ...config,
+      }).then((response) => {
+        return Promise.resolve(response)
+      }).catch((error) => {
+        return Promise.reject(error)
+      })
+    }
+  }
+}
